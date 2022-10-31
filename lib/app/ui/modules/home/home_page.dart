5bb837985a21ddef2035bde2d444315aabb3a151
Lion Hofman <lion.hofman@posthaus.com.br>
2022-10-31 08:40:43 -0300

    🚧wip: banners home

--- lib/app/ui/modules/home/home_page.dart
+++ lib/app/ui/modules/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -17,12 +18,63 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
+      // appBar: AppBar(
+      //   title: const Text('Home Page'),
+      // ),
+
+      /**
+       * 
+       * TODO Buscar as informações atraves da camada do DataSource e vir trazendo pelas camadas sem deixar uma consulta no banco de dados direto no front-end
+       */
       body: Stack(
         children: [
           _buildBodyBack(),
+          CustomScrollView(
+            slivers: [
+              const SliverAppBar(
+                floating: true,
+                snap: true,
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: Text("Novidades"),
+                  centerTitle: true,
+                ),
+              ),
+              FutureBuilder<QuerySnapshot>(
+                future: FirebaseFirestore.instance
+                    .collection("home")
+                    .orderBy("pos")
+                    .get(),
+                builder: ((context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return SliverToBoxAdapter(
+                      child: Container(
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                        ),
+                      ),
+                    );
+                  } else {
+                    print(snapshot.data!.docs.length);
+                    return SliverToBoxAdapter(
+                      child: Container(
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                        ),
+                      ),
+                    );
+                  }
+                }),
+              )
+            ],
+          )
         ],
       ),
     );
